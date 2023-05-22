2d7d1aa7be21ad1df9cb4afd78bc5a3180811b68
Ahmet Iclier <ahmet.iclier@icloud.com>
2023-05-22 12:52:44 +0200

    added css and js

--- main.js
+++ main.js
@@ -3,15 +3,16 @@
 let currentPokemon;
 
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    let url = 'https://pokeapi.co/api/v2/pokemon/6/';
     let response = await fetch(url);
     currentPokemon = await response.json();
 
-    console.log(responseAsJson);
+    console.log('Loaded Pokemon', currentPokemon);
 
     renderPokemonInfo()
 }
 
 function renderPokemonInfo(){
-    
+    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+    document.getElementById('pokeImage').src = currentPokemon['sprites']['front_default'];
 }
