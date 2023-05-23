dfe099db4a36696f0b46e62b5f06dfdb7a5f7bbe
Ahmet Iclier <ahmet.iclier@icloud.com>
2023-05-23 11:26:43 +0200

    added renderfunctions and css

--- main.js
+++ main.js
@@ -3,16 +3,49 @@
 let currentPokemon;
 
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/6/';
-    let response = await fetch(url);
-    currentPokemon = await response.json();
-
-    console.log('Loaded Pokemon', currentPokemon);
-
-    renderPokemonInfo()
+    for (let i = 1; i<152; i++) {
+        const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+        const pokemonData = await pokemon.json();
+        document.getElementById('pokeContainer').innerHTML += renderCard(pokemonData, i)
+        renderTypes(pokemonData, i)
+    }
+    
 }
 
-function renderPokemonInfo(){
-    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
-    document.getElementById('pokeImage').src = currentPokemon['sprites']['front_default'];
+function renderCard(pokemonData, j) {
+    return `
+    <div class="miniCard ${renderBGColor(pokemonData)}" id="pokeCard(${j})" onclick="openCard(${j})">
+        <div class="left">
+            <h3>${renderName(pokemonData)}</h3>
+            <div class="typeBox" id="typeBox${j}"></div>
+        </div>
+        <div class="right">
+            <h4>#${renderPokeID(pokemonData)}</h4>
+            <img src="${renderIMG(pokemonData)}"> 
+        </div>
+    </div>
+`
+}
+
+function renderPokeID(pokemonData) {
+    return pokemonData.id;
+}
+
+function renderBGColor(pokemonData) {
+    return pokemonData['types']['0']['type']['name'];
+}
+
+function renderIMG(pokemonData) {
+    return pokemonData['sprites']['other']['home']['front_default'];
+}
+
+function renderTypes(pokemonData, j) {
+    for (let i = 0; i<pokemonData['types'].length; i++){
+        document.getElementById(`typeBox${j}`).innerHTML += `<span class="tag">${pokemonData['types'][i]['type']['name']}</span><br>`;
+    }
+}
+
+function renderName (pokemonData) {
+    let name = pokemonData['name'];
+    return name.charAt(0).toUpperCase() + name.slice(1);
 }
